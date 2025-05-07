ade67b8009afb1d10c48ae287221a4773420b953
hanaalkadri <hanaalkadry@gmail.com>
2025-05-08 00:22:20 +0200

    Add Hero section with layout and images for Shop page

--- src/Components/Assets/Navbar/Navbar.jsx
+++ src/Components/Assets/Navbar/Navbar.jsx
@@ -16,19 +16,19 @@ const Navbar = () => {
 
       <ul className="nav-menu">
         <li onClick={() => setMenu("shop")}>
-          <Link to='/'>Shop</Link>
+          <Link style={{textDecoration:'none'}} to='/'>Shop</Link>
           {menu === "shop" && <hr />}
         </li>
         <li onClick={() => setMenu("men")}>
-          <Link to='/men'>Men</Link>
+          <Link style={{textDecoration:'none'}} to='/men'>Men</Link>
           {menu === "men" && <hr />}
         </li>
         <li onClick={() => setMenu("women")}>
-          <Link to='/women'>Women</Link>
+          <Link style={{textDecoration:'none'}} to='/women'>Women</Link>
           {menu === "women" && <hr />}
         </li>
         <li onClick={() => setMenu("kids")}>
-          <Link to='/kids'>Kids</Link>
+          <Link style={{textDecoration:'none'}} to='/kids'>Kids</Link>
           {menu === "kids" && <hr />}
         </li>
       </ul>
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
+import Hero from '../Components/Hero/Hero';
 const Shop = () => {
   return (
     <div>
-      <h1>Welcome to the Shop</h1>
-      <p>Browse our products by category.</p>
+    <Hero/>
     </div>
   );
 };
