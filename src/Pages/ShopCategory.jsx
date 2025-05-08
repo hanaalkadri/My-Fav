17cacbee5cabcfe888f82e362b3474d679589434
hanaalkadri <hanaalkadry@gmail.com>
2025-05-08 22:35:03 +0200

    Fix category filter in ShopCategory and render items by category

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/item';
 
-const ShopCategory = ({ category }) => {
+const ShopCategory = (props) => {
+  const { all_Product } = useContext(ShopContext);
+
   return (
-    <div>
-      <h1>Category: {category}</h1>
-      <p>Products under the {category} category will be displayed here.</p>
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+      <div className="shopcategoru-indexSort">
+        <p>
+          <span>
+            Showing 1-12 out of 36 products
+          </span>
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+
+      <div className="shopcategory-products">
+        {all_Product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // لو كان عندك ملف CSS عام
+import ShopContextProvider from './Context/ShopContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ShopContextProvider>
     <App />
-  </React.StrictMode>
+  </ShopContextProvider>
 );
