157ec3cfb14310f711c7ba6549b52f48d0bb7ac4
hanaalkadri <hanaalkadry@gmail.com>
2025-05-13 12:32:11 +0200

    Add ShopContext with cart logic and product data

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_Product from "../Components/Assets/all_product";
 
 
@@ -6,9 +6,26 @@ import all_Product from "../Components/Assets/all_product";
 
 export const ShopContext= createContext(null);
 
-const ShopContextProvider = (props) =>{
-    const contextValue={all_Product}
+const getDefaultCart=()=>{
+    let cart={};
+    for(let index = 0; index < all_Product.length+1; index++){
+       cart[index] = 0;
+    }
+    return cart;
+}
 
+const ShopContextProvider = (props) =>{
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+   
+
+    const addToCart = (itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+    }
+
+    const removeFromCart = (itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+    }
+    const contextValue={all_Product, cartItems}
     return(
         <ShopContext.Provider value={contextValue}>
             {props.children}
