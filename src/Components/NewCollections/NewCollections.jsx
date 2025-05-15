95650930f9e83339a41f07408b9f58c6ae7a0f9a
hanaalkadri <hanaalkadry@gmail.com>
2025-05-15 14:26:19 +0200

    Update new-collections styles and media queries

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,30 +1,20 @@
-import React from 'react';
-import '../Popular/Popular.css'; 
-import './NewCollections.css';
-import new_collection from '../Assets/new_collections';
-import Item from '../Item/item';
+import React from 'react'
+import './NewCollections.css'
+import new_collection from '../Assets/new_collections'
+import Item from '../Item/item'
 
 const NewCollections = () => {
   return (
-    <div className='popular'>
-      <h1>New Collections</h1>
+    <div className='new-collections'>
+      <h1>NEW COLLECTIONS</h1>
       <hr />
-      <div className="popular-item">
-        {new_collection.map((item, i) => {
-          return (
-            <Item
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              new_price={item.new_price}
-              old_price={item.old_price}
-            />
-          );
+      <div className="collections">
+        {new_collection.map((item,i)=>{
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NewCollections;
+export default NewCollections
